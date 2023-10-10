f955927e082e280572ce62e1bb311e3870d27c8d
calebgp <caleb.gp.dev@gmail.com>
2023-10-09 21:44:25 -0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,24 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: ChatsPage());
+    Map<int, Color> color = {
+      50: const Color.fromRGBO(136, 14, 79, .1),
+      100: const Color.fromRGBO(136, 14, 79, .2),
+      200: const Color.fromRGBO(136, 14, 79, .3),
+      300: const Color.fromRGBO(136, 14, 79, .4),
+      400: const Color.fromRGBO(136, 14, 79, .5),
+      500: const Color.fromRGBO(136, 14, 79, .6),
+      600: const Color.fromRGBO(136, 14, 79, .7),
+      700: const Color.fromRGBO(136, 14, 79, .8),
+      800: const Color.fromRGBO(136, 14, 79, .9),
+      900: const Color.fromRGBO(136, 14, 79, 1),
+    };
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const ChatsPage(),
+      theme: ThemeData(
+        primarySwatch: MaterialColor(0xFF008069, color),
+      ),
+    );
   }
 }
--- lib/screens/chats_page.dart
+++ lib/screens/chats_page.dart
@@ -1,12 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ChatsPage extends StatelessWidget {
   const ChatsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(backgroundColor: const Color(0xFF008069)),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            toolbarHeight: 58,
+            title: Text(
+              'WhatsApp',
+              style: GoogleFonts.roboto(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.camera_alt_outlined,
+                  color: Colors.white,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.more_vert,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+            bottom: TabBar(
+              indicatorColor: Colors.black,
+              indicatorWeight: 2,
+              labelStyle:
+                  GoogleFonts.roboto(fontSize: 14, fontWeight: FontWeight.w700),
+              tabs: [
+                Tab(
+                  text: "CHATS",
+                ),
+                Tab(
+                  text: "STATUS",
+                ),
+                Tab(
+                  text: "CALLS",
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [],
+          )),
     );
   }
 }
